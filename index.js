49504594c1fd0a8ba39e0ba22fb95e94f910302c
USERDOMAIN\bollini <bollini.alessandro1998@gmail.com>
2023-01-16 10:46:23 +0100

    c

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/', encodeUrl, async (req, res) => {
     const alreadyExist = await controller.checkUser(req.body.userAddress);
     try {
         if (alreadyExist == 0) {
-            await exec('curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -sudo apt-get install -y nodejs; npx hardhat run scripts/deploy.js --network mumbai', { 
+            await exec('npx hardhat run scripts/deploy.js --network mumbai', { 
                 env: { USERWALLET: req.body.userAddress, EMAIL: req.body.email }, function(error, stdout, stderr) {
                     if (error !== null) {
                         console.log('exec error: ', error);
